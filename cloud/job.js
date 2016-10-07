4d28576a5fef5ee7f5440b7af63284eace76ad33
alexyesiam <alexyesiam@gmail.com>
2016-10-07 16:49:03 -0700

    Update job.js

--- cloud/job.js
+++ cloud/job.js
@@ -3,21 +3,18 @@ Parse.initialize('blindbox', 'MrFF6pmuI0ibpUheixmd', 'n5e0v9u2DxjkLWPmgQP8');
 Parse.serverURL = 'http://blindbox.herokuapp.com/parse/';
 Parse.Cloud.useMasterKey();
 
-var GameScore = Parse.Object.extend("GameScore");
-var gameScore = new GameScore();
-
-gameScore.set("score", 1337);
-gameScore.set("playerName", "Sean Plott");
-gameScore.set("cheatMode", false);
-
-gameScore.save(null, {
-  success: function(gameScore) {
-    // Execute any logic that should take place after the object is saved.
-    alert('New object created with objectId: ' + gameScore.id);
-  },
-  error: function(gameScore, error) {
-    // Execute any logic that should take place if the save fails.
-    // error is a Parse.Error with an error code and message.
-    alert('Failed to create new object, with error code: ' + error.message);
-  }
+function saveSomething(){
+var MyClass = Parse.Object.extend("MyClass");
+var myclass = new MyClass();
+myclass.set("columnName", "value");
+myclass.save({
+    success: function(place){
+        console.log("Success!!");
+    },
+    error: function(place, error){
+        console.log("Fail: " + error.message);
+    }
 });
+}
+
+saveSomething();
