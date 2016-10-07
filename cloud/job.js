9baf33381c740b9ce6cb7f3d6fbe790f56f902a0
alexyesiam <alexyesiam@gmail.com>
2016-10-07 16:38:31 -0700

    Update job.js

--- cloud/job.js
+++ cloud/job.js
@@ -3,18 +3,21 @@ Parse.initialize('blindbox', 'MrFF6pmuI0ibpUheixmd', 'n5e0v9u2DxjkLWPmgQP8');
 Parse.serverURL = 'http://blindbox.herokuapp.com/parse/';
 Parse.Cloud.useMasterKey();
 
-    var today = new Date();
-    var days = 1;
-    var time = (days * 24 * 3600 * 1000);
-    var expirationDate = new Date(today.getTime() - (time));
+var GameScore = Parse.Object.extend("GameScore");
+var gameScore = new GameScore();
 
-    var query = new Parse.Query('Groups');
-    query.each(function(Groups) {
-        return Groups.destroy();
-    }).then(function() {
-        console.log("Delete job completed.");
-        status.success("Delete job completed.");
-    }, function(error) {
-        alert("Error: " + error.code + " " + error.message);
-        status.error("Error: " + error.code + " " + error.message);
-    });
+gameScore.set("score", 1337);
+gameScore.set("playerName", "Sean Plott");
+gameScore.set("cheatMode", false);
+
+gameScore.save(null, {
+  success: function(gameScore) {
+    // Execute any logic that should take place after the object is saved.
+    alert('New object created with objectId: ' + gameScore.id);
+  },
+  error: function(gameScore, error) {
+    // Execute any logic that should take place if the save fails.
+    // error is a Parse.Error with an error code and message.
+    alert('Failed to create new object, with error code: ' + error.message);
+  }
+});
