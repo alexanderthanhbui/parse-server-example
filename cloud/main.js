2f9cb07d69a1723e2cc5eb78432c29904b423150
alexyesiam <alexyesiam@gmail.com>
2016-10-07 14:07:50 -0700

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,39 @@
+var Parse = require('parse/node');
+Parse.initialize('blindbox', 'MrFF6pmuI0ibpUheixmd', 'n5e0v9u2DxjkLWPmgQP8');
+Parse.serverURL = 'http://blindbox.herokuapp.com/parse/';
+Parse.Cloud.useMasterKey();
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+Parse.Cloud.job("deleteMessages", function(request, status) 
+
+Parse.Cloud.useMasterKey();
+
+ var ts = Math.round(new Date().getTime() / 1000);
+var tsYesterday = ts - (24 * 3600);
+var dateYesterday = new Date(tsYesterday*1000);
+
+var query = new Parse.Query("Groups");
+
+query.lessThan("createdAt", dateYesterday);
+
+query.find({
+    success: function(result) {
+        for(var i=0; i<result.length; i++) {
+            result[i].destroy({
+                success: function(object) {
+                    status.success("Delete job completed");
+                    alert('Delete Successful');
+                },
+                error: function(object, error) {
+                    status.error("Delete error :" + error);
+                    alert('Delete failed');
+                }
+            });
+        }
+        status.success("Delete job completed");
+    },
+    error: function(error) {
+        status.error("Error in delete query error: " + error);
+        alert('Error in delete query');
+    }
+});
 });
