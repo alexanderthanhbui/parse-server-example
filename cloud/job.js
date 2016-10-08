5a84d4c84fc3221c2eaaa089777d891a9fe6bc42
alexyesiam <alexyesiam@gmail.com>
2016-10-08 00:23:02 -0700

    Update job.js

--- cloud/job.js
+++ cloud/job.js
@@ -11,18 +11,7 @@ gameScore.set("score", 1337);
 gameScore.set("playerName", "Sean Plott");
 gameScore.set("cheatMode", false);
 
-gameScore.save(null,{useMasterKey: true}); {
-
-  success: function(gameScore) {
-    // Execute any logic that should take place after the object is saved.
-    alert('New object created with objectId: ' + gameScore.id);
-  },
-  error: function(gameScore, error) {
-    // Execute any logic that should take place if the save fails.
-    // error is a Parse.Error with an error code and message.
-    alert('Failed to create new object, with error code: ' + error.message);
-  }
-});
+gameScore.save(null,{useMasterKey: true});
 }
 
 function sayHello() {
