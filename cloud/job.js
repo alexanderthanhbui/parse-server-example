862724f4dd66a974d256e9b54cabbb911fad82aa
alexyesiam <alexyesiam@gmail.com>
2016-10-07 18:59:33 -0700

    Update job.js

--- cloud/job.js
+++ cloud/job.js
@@ -3,19 +3,21 @@ Parse.initialize('blindbox', 'n5e0v9u2DxjkLWPmgQP8', 'n5e0v9u2DxjkLWPmgQP8');
 Parse.serverURL = 'http://blinbox.herokuapp.com/parse';
 Parse.Cloud.useMasterKey();
 
-    var today = new Date();
-    var days = 1;
-    var time = (days * 24 * 3600 * 1000);
-    var expirationDate = new Date(today.getTime() - (time));
+function saveSomething(){
+var obj = new Parse.Object('my_class', 'random');
+obj.save(null, {
+    success: function(place){
+        console.log("Success!!");
+    },
+    error: function(place, error){
+        console.log("Fail: " + error.message);
+    }
+});
+}
 
-    var query = new Parse.Query('Groups');
-    query.lessThan('createdAt', expirationDate);
-    query.each(function(Groups) {
-        return Groups.destroy();
-    }).then(function() {
-        console.log("Delete job completed.");
-        status.success("Delete job completed.");
-    }, function(error) {
-        alert("Error: " + error.code + " " + error.message);
-        status.error("Error: " + error.code + " " + error.message);
-    });
+function sayHello() {
+console.log('Hello');
+}
+
+sayHello();
+saveSomething();
