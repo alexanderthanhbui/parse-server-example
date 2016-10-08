0b4805fe5c6064f550b9e965ba9eb652210d03ff
alexyesiam <alexyesiam@gmail.com>
2016-10-08 00:11:33 -0700

    Update job.js

--- cloud/job.js
+++ cloud/job.js
@@ -4,14 +4,23 @@ Parse.serverURL = 'http://blinbox.herokuapp.com/parse';
 Parse.Cloud.useMasterKey();
 
 function saveSomething(){
-var obj = new Parse.Object('my_class', 'random');
-obj.save(null, {
-    success: function(place){
-        console.log("Success!!");
-    },
-    error: function(place, error){
-        console.log("Fail: " + error.message);
-    }
+var GameScore = Parse.Object.extend("GameScore");
+var gameScore = new GameScore();
+
+gameScore.set("score", 1337);
+gameScore.set("playerName", "Sean Plott");
+gameScore.set("cheatMode", false);
+
+gameScore.save(null, {
+  success: function(gameScore) {
+    // Execute any logic that should take place after the object is saved.
+    alert('New object created with objectId: ' + gameScore.id);
+  },
+  error: function(gameScore, error) {
+    // Execute any logic that should take place if the save fails.
+    // error is a Parse.Error with an error code and message.
+    alert('Failed to create new object, with error code: ' + error.message);
+  }
 });
 }
 
