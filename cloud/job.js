be057e3723230292c7a95d13284d01234d86064e
alexyesiam <alexyesiam@gmail.com>
2016-10-07 18:21:50 -0700

    Update job.js

--- cloud/job.js
+++ cloud/job.js
@@ -1,8 +1,7 @@
 
 var Parse = require('parse/node');
-Parse.initialize('blindbox','n5e0v9u2DxjkLWPmgQP8');
+Parse.initialize('blindbox');
 Parse.serverURL = 'http://blindbox.herokuapp.com/parse';
-Parse.Cloud.useMasterKey();
 
 function saveSomething(){
 var MyClass = Parse.Object.extend("MyClass");
