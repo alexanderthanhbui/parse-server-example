da1a0a6a5644f4a21e5f9b6b5e80c7c83326a6ea
Alexander Bui <alexyesiam@gmail.com>
2017-08-11 01:37:03 -0700

    Update index.js

--- index.js
+++ index.js
@@ -18,16 +18,12 @@ var api = new ParseServer({
   masterKey: process.env.MASTER_KEY || 'n5e0v9u2DxjkLWPmgQP8', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://blindbox.herokuapp.com/parse', // Don't forget to change to https if needed
   push: {
-    ios: [{
-      pfx: __dirname + 'pusherDev.p12', // Dev PFX or P12
-      bundleId: 'com.animenim.Yipster',
-      production: false // Dev
-    }, {
+    ios: {
       pfx: __dirname + 'pusher.p12', // Prod PFX or P12
       passphrase: 'alexbui1', // optional password to your p12/PFX
       bundleId: 'com.animenim.Yipster',
       production: true // Prod
-    }]
+    }
   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
