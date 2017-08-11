67771b36af9a5f04612ccc5722aee8f618a8e7e1
Alexander Bui <alexyesiam@gmail.com>
2017-08-11 00:13:24 -0700

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,38 +1,23 @@
-  Parse.Cloud.define("pushToFollowers", function (request, response) {
-  console.log("Inside pushToFollowers");
-  var params = request.params;
+// iOS push testing
+Parse.Cloud.define("iosPush", function(request, response) {
+ 
   var user = request.user;
-  var sessionToken = user.getSessionToken();
-  var someKey = request.someKey;
-  var data = params.data;
-
- // Set our query to target specific user 
-  var recipientUser = new Parse.Query(Parse.User);
-  recipientUser.equalTo("objectId", request.params.someKey);
-
-// Set our installation query
+  var params = request.params;
+  var someKey = params.someKey
+  var data = params.data
+ 
   var pushQuery = new Parse.Query(Parse.Installation);
-   pushQuery.equalTo('deviceType', 'ios');
-   pushQuery.matchesQuery('user', recipientUser); 
-   pushQuery.find({ useMasterKey: true }).then(function(object) {
-        response.success(object); 
-        console.log("pushQuery got " + object.length);
-        }, function(error) {
-          response.error(error);
-            console.error("pushQuery find failed. error = " + error.message);
-    });
-
-  // Send push notification to query
+  pushQuery.equalTo('deviceType', 'ios'); // targeting iOS devices only
+  pushQuery.equalTo("someKey", someKey)
+ 
   Parse.Push.send({
-    where: pushQuery,
-    data: data  
-  }, { useMasterKey: true })
-    .then(function() {
-      // Push sent!
-      console.log("#### push sent!");
-    }, function(error) {
-      // There was a problem :(
-      console.error("#### push error" + error.message);
-    });
-response.success('success, end of pushToFollowers')
+    where: pushQuery, // Set our Installation query
+    data: data
+  }, { success: function() {
+      console.log("#### PUSH OK");
+  }, error: function(error) {
+      console.log("#### PUSH ERROR" + error.message);
+  }, useMasterKey: true});
+ 
+  response.success('success');
 });
