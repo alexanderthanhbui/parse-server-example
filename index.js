4bc4ba77df1aa977134d3edf7aab47854fa09b79
Alexander Bui <alexyesiam@gmail.com>
2017-08-11 02:38:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://blindbox.herokuapp.com/parse', // Don't forget to change to https if needed
   push: {
     ios: {
-      pfx: 'pusher.p12', // Prod PFX or P12
-      passphrase: 'alexbui1', // optional password to your p12/PFX
+      pfx: 'pusherDev.p12', // Prod PFX or P12
+     // passphrase: 'alexbui1', // optional password to your p12/PFX
       bundleId: 'com.animenim.Yipster',
-      production: true // Prod
+      production: false // Prod
     }
   }
 });
